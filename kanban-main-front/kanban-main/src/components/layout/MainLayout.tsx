942ccf19b91318d761d9be8c963e0c1fdffe7a5c
Eman Abukhater <Eman-Abukhater@users.noreply.github.com>
2026-01-20 16:12:23 +0300

    fix the v6 review except image upload

--- kanban-main-front/kanban-main/src/components/layout/MainLayout.tsx
+++ kanban-main-front/kanban-main/src/components/layout/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { Footer } from "./Footer";
 import { MainContent } from "./MainContent";
-import { Navbar } from "./Navbar";
 
 export interface IMainLayoutProps {}
 
 export function MainLayout(props: IMainLayoutProps) {
   return (
     <>
-      <Navbar />
       <div className="relative flex min-h-screen flex-col px-4 pt-14 sm:px-6 sm:py-8">
         <MainContent />
         <Footer />
